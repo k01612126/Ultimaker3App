569e5abbc3e4c501363e2947afc1e223391800cd
MuehlburerFlorian <muehlburger.florian@gmail.com>
2019-01-21 16:08:29 +0100

    änderungen stefan

--- Ultimaker3App/frontend/src/app/services/ultimaker-service.ts
+++ Ultimaker3App/frontend/src/app/services/ultimaker-service.ts
@@ -1,4 +1,4 @@
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
 import {Injectable} from "@angular/core";
 import {SystemProperties} from "../shared/SystemProperties";
@@ -47,9 +47,10 @@ export class UltimakerService {
   }
 
   verifyAuthentication(): Observable<any> {
-    return this.http.get<any>(this.apiUrl + "/auth/verify");
-  }
 
+    return this.http.get<any>(this.apiUrl + "/auth/verify",{headers: new HttpHeaders().set('Authorization', 'Digest username="25d73a1e8f4cb0f9f120620ff465abf9", realm="Jedi-API", nonce="ea5e7a5a9b6b14e80d8cb6cfb6f2a8af", uri="/api/v1/auth/verify", response="d23e4e313d43bf7013048026321ef906", qop=auth, nc=00000011, cnonce="2eb96d94a90ec26d"')
+        .set('Content-Type', 'application/json')});
+  }
   letItBlink(): Observable<any> {
     return this.http.post<any>(this.apiUrl + "/printer/led/blink", {
       frequency: 50,
